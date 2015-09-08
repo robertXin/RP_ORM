eaee2057888dba4f673e12fa475a13ec08ed88f9
luox04 <luox04@mysoft.com.cn>
2015-09-08 17:03:44 +0800

    兼容php5.5.28，实现结果集直接返回数组，实现方法getUniPri获取表的主键

--- rp_orm.c
+++ rp_orm.c
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2010 The PHP Group                                |
+  | Copyright (c) 1997-2015 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: header 297205 2010-03-30 21:09:07Z johannes $ */
+/* $Id$ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -35,38 +35,28 @@ ZEND_DECLARE_MODULE_GLOBALS(rp_orm)
 
 /* True global resources - no need for thread safety here */
 static int le_rp_orm;
-zend_fcall_info fci;
 zend_class_entry *ormclass_ce;
-int luoxin_call_user_method(zval** retval, zval* obj, char* function_name, char* paras, ...);
+zend_fcall_info luoxin_call_user_method(zval** retval, zval* obj, char* function_name, char* paras, ...);
 void pdo_query(zval* return_value, zval* pdo_obj,char* sql,int sql_len TSRMLS_DC);
 
-/* {{{ rp_orm_functions[]
- *
- * Every user visible function must have an entry in rp_orm_functions[].
- */
-const zend_function_entry rp_orm_functions[] = {
-	PHP_FE(test_pdo,	NULL)		/* For testing, remove later. */
-	{NULL, NULL, NULL}	/* Must be the last line in rp_orm_functions[] */
-};
-/* }}} */
 
 /* {{{ rp_orm_module_entry
  */
 zend_module_entry rp_orm_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
-	STANDARD_MODULE_HEADER,
+    STANDARD_MODULE_HEADER,
 #endif
-	"rp_orm",
-	rp_orm_functions,
-	PHP_MINIT(rp_orm),
-	PHP_MSHUTDOWN(rp_orm),
-	PHP_RINIT(rp_orm),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(rp_orm),	/* Replace with NULL if there's nothing to do at request end */
-	PHP_MINFO(rp_orm),
+    "rp_orm",
+    NULL,
+    PHP_MINIT(rp_orm),
+    PHP_MSHUTDOWN(rp_orm),
+    PHP_RINIT(rp_orm),      /* Replace with NULL if there's nothing to do at request start */
+    PHP_RSHUTDOWN(rp_orm),  /* Replace with NULL if there's nothing to do at request end */
+    PHP_MINFO(rp_orm),
 #if ZEND_MODULE_API_NO >= 20010901
-	"0.1", /* Replace with version number for your extension */
+    PHP_RP_ORM_VERSION,
 #endif
-	STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_PROPERTIES
 };
 /* }}} */
 
@@ -89,8 +79,8 @@ PHP_INI_END()
 /* Uncomment this function if you have INI entries
 static void php_rp_orm_init_globals(zend_rp_orm_globals *rp_orm_globals)
 {
-	rp_orm_globals->global_value = 0;
-	rp_orm_globals->global_string = NULL;
+    rp_orm_globals->global_value = 0;
+    rp_orm_globals->global_string = NULL;
 }
 */
 /* }}} */
@@ -110,12 +100,31 @@ ZEND_METHOD(ormclass,query)
     pdo_query(return_value,obj,Z_STRVAL_P(sql),Z_STRLEN_P(sql) TSRMLS_DC);
 }
 
+/* get unique primary columnname*/
+ZEND_METHOD(ormclass,getUniPri)
+{
+    zend_class_entry *ce;
+    zval *obj,*tableName;
+    char sql[100];
+
+    ce = Z_OBJCE_P(getThis());
+    obj = zend_read_property(ce,getThis(),"pdo_obj",sizeof("pdo_obj") - 1,0 TSRMLS_CC);
+    // obj = zend_read_static_property(ce,"pdo_obj",sizeof("pdo_obj") - 1,0 TSRMLS_CC);
+    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,"z",&tableName) == FAILURE)
+    {
+        RETURN_NULL();
+    }
+    sprintf(sql,"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME='%s';",Z_STRVAL_P(tableName));
+    // printf("%s",sql);
+    pdo_query(return_value,obj,sql,sizeof(sql)-1 TSRMLS_DC);
+}
+
 ZEND_METHOD(ormclass,__construct)
 {
     zval *config;
     zend_class_entry *ce;
-    int count,i,port;
-    char *s,*h,*dbname,*username,*password;
+    int count,i;
+    char *s,*h,*dbname,*username,*password,*port;
     char dns[100];
     zval **z_item;
 
@@ -164,7 +173,7 @@ ZEND_METHOD(ormclass,__construct)
     pdo_dbh_ce = php_pdo_get_dbh_ce();
     object_init_ex(obj, pdo_dbh_ce);
 
-
+    // printf("%d",167);
     luoxin_call_user_method(NULL,obj,"__construct","sss",&dns,sizeof(dns)-1,username,strlen(username),password,strlen(password));
 
     // zend_update_static_property(ce,"pdo_obj",sizeof("pdo_obj") - 1,obj TSRMLS_CC);
@@ -174,11 +183,11 @@ ZEND_METHOD(ormclass,__construct)
 
 zend_function_entry ormclass_method[]=
 {
-    ZEND_ME(ormclass,query,NULL,ZEND_ACC_PUBLIC)
-    ZEND_ME(ormclass,__construct,NULL,ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
-    {NULL, NULL, NULL}
+    PHP_ME(ormclass,query,NULL,ZEND_ACC_PUBLIC)
+    PHP_ME(ormclass,getUniPri,NULL,ZEND_ACC_PUBLIC)
+    PHP_ME(ormclass,__construct,NULL,ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
+    PHP_FE_END
 };
-
 
 /* {{{ PHP_MINIT_FUNCTION
  */
@@ -190,12 +199,10 @@ PHP_MINIT_FUNCTION(rp_orm)
     ormclass_ce = zend_register_internal_class(&orm_ce TSRMLS_CC);
     zend_declare_property_null(ormclass_ce,"pdo_obj",strlen("pdo_obj"),ZEND_ACC_PUBLIC TSRMLS_CC);
 
-
-	// PHP_MINIT_FUNCTION(pdo);
-	/* If you have INI entries, uncomment these lines 
-	REGISTER_INI_ENTRIES();
-	*/
-	return SUCCESS;
+    /* If you have INI entries, uncomment these lines 
+    REGISTER_INI_ENTRIES();
+    */
+    return SUCCESS;
 }
 /* }}} */
 
@@ -203,10 +210,10 @@ PHP_MINIT_FUNCTION(rp_orm)
  */
 PHP_MSHUTDOWN_FUNCTION(rp_orm)
 {
-	/* uncomment this line if you have INI entries
-	UNREGISTER_INI_ENTRIES();
-	*/
-	return SUCCESS;
+    /* uncomment this line if you have INI entries
+    UNREGISTER_INI_ENTRIES();
+    */
+    return SUCCESS;
 }
 /* }}} */
 
@@ -215,7 +222,7 @@ PHP_MSHUTDOWN_FUNCTION(rp_orm)
  */
 PHP_RINIT_FUNCTION(rp_orm)
 {
-	return SUCCESS;
+    return SUCCESS;
 }
 /* }}} */
 
@@ -224,7 +231,7 @@ PHP_RINIT_FUNCTION(rp_orm)
  */
 PHP_RSHUTDOWN_FUNCTION(rp_orm)
 {
-	return SUCCESS;
+    return SUCCESS;
 }
 /* }}} */
 
@@ -232,115 +239,36 @@ PHP_RSHUTDOWN_FUNCTION(rp_orm)
  */
 PHP_MINFO_FUNCTION(rp_orm)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "rp_orm support", "enabled");
-	php_info_print_table_end();
+    php_info_print_table_start();
+    php_info_print_table_header(2, "rp_orm support", "enabled");
+    php_info_print_table_end();
 
-	/* Remove comments if you have entries in php.ini
-	DISPLAY_INI_ENTRIES();
-	*/
+    /* Remove comments if you have entries in php.ini
+    DISPLAY_INI_ENTRIES();
+    */
 }
 /* }}} */
 
 
-/* Remove the following function when you have succesfully modified config.m4
+/* Remove the following function when you have successfully modified config.m4
    so that your module can be compiled into PHP, it exists only for testing
    purposes. */
 
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_rp_orm_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
-PHP_FUNCTION(test_pdo)
-{
-    zval *obj,*pobj,*arr;
-    HashTable *myht;
-    // uint count;
-    MAKE_STD_ZVAL(obj);
-    MAKE_STD_ZVAL(pobj);
 
-    int count, i;
-    zval **z_item;
-    ulong index;
-
-    zend_class_entry *pdo_dbh_ce;
-    pdo_dbh_ce = php_pdo_get_dbh_ce();
-    object_init_ex(obj, pdo_dbh_ce);
-
-    luoxin_call_user_method(NULL,obj,"__construct","sss","mysql:dbname=test;host=localhost",sizeof("mysql:dbname=test;host=localhost") -1,"xxx",sizeof("xxx") - 1,"xxxx",sizeof("xxxxx") -1);
-    luoxin_call_user_method(NULL,obj,"query","s","select * from test",sizeof("select * from test") - 1);
-    COPY_PZVAL_TO_ZVAL(*pobj,*fci.retval_ptr_ptr);
-    // efree(fci);
-    ALLOC_ZVAL(arr);
-    while(1){
-        luoxin_call_user_method(NULL,pobj,"fetch","l",PDO_FETCH_ASSOC);
-
-        if(Z_LVAL_P(*fci.retval_ptr_ptr) == 0)
-            break;
-        COPY_PZVAL_TO_ZVAL(*arr, *fci.retval_ptr_ptr);
-        count = zend_hash_num_elements(Z_ARRVAL_P(arr));
-        zend_hash_internal_pointer_reset(Z_ARRVAL_P(arr)); 
-
-        for (i = 0; i < count; i ++) {
-            char* key;
-            ulong idx;
-            zend_hash_get_current_data(Z_ARRVAL_P(arr), (void**) &z_item);
-            convert_to_string_ex(z_item);
-            if (zend_hash_get_current_key(Z_ARRVAL_P(arr), &key, &idx, 0) == HASH_KEY_IS_STRING) {
-            // KEY爲字符串
-            } else {
-            }
-            // 將數組中的內部指針向前移動一位
-            zend_hash_move_forward(Z_ARRVAL_P(arr));
-        }
-    // COPY_PZVAL_TO_ZVAL(*return_value, *fci.retval_ptr_ptr);
-    }
-    // while(1){
-    //     luoxin_call_user_method(NULL,pobj,"fetch","l",PDO_FETCH_ASSOC);
-    //     COPY_PZVAL_TO_ZVAL(*arr, *fci.retval_ptr_ptr);
-    //     if(Z_LVAL_P(arr) == 0)
-    //         break;
-
-    //     COPY_PZVAL_TO_ZVAL(*return_value, arr);
-
-    //     // myht = Z_ARRVAL_P(arr);
-    //     // zend_hash_internal_pointer_reset_ex(ht, &iterator);
-    //     // while (zend_hash_get_current_data_ex(ht, (void **) &tmp, &iterator) == SUCCESS) {
-    //     //     zend_hash_get_current_key_ex(ht, &string_key, &str_len, &num_key, 0, &iterator)
-    //     // } 
-    // }
-    // luoxin_call_user_method(NULL,pobj,"getColumnMeta","l",0);
-    // COPY_PZVAL_TO_ZVAL(*return_value, *fci.retval_ptr_ptr);
-    
-    // luoxin_call_user_method(NULL,obj,"call_hello","");
-    if (fci.params) {
-        efree(fci.params);
-    }
-    zval_ptr_dtor(&obj);
-    return;
-}
-/* }}} */
 /* The previous line is meant for vim and emacs, so it can correctly fold and 
    unfold functions in source code. See the corresponding marks just before 
    function definition, where the functions purpose is also documented. Please 
    follow this convention for the convenience of others editing your code.
 */
 
-
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * End:
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
- */
-
 void pdo_query(zval* return_value,zval* pdo_obj,char* sql, int sql_len TSRMLS_DC){
-    luoxin_call_user_method(NULL,pdo_obj,"query","s",sql,sql_len);
-    COPY_PZVAL_TO_ZVAL(*return_value, *fci.retval_ptr_ptr);
+    zend_fcall_info fci,fci2;
+    fci = luoxin_call_user_method(NULL,pdo_obj,"query","s",sql,sql_len);
+    fci2 = luoxin_call_user_method(NULL,*fci.retval_ptr_ptr,"fetchAll","l",2);
+    COPY_PZVAL_TO_ZVAL(*return_value, *fci2.retval_ptr_ptr);
 }
 
-int luoxin_call_user_method(zval** retval, zval* obj, char* function_name, char* paras, ...)
+zend_fcall_info luoxin_call_user_method(zval** retval, zval* obj, char* function_name, char* paras, ...)
 {
     //用于接收参数
     short int paras_count=0;
@@ -351,9 +279,11 @@ int luoxin_call_user_method(zval** retval, zval* obj, char* function_name, char*
     zval *zval_tmp;
     double dou_tmp;
     int i;
+    zend_fcall_info fci;
+    fci.retval_ptr_ptr = 0;
 
     //仅与调用有关的变量
-    int fun_re, retval_is_null=0;
+    int retval_is_null=0;
     HashTable *function_table;
 
     //接收参数
@@ -406,7 +336,7 @@ int luoxin_call_user_method(zval** retval, zval* obj, char* function_name, char*
                     break;
                 default:
                     zend_error(E_ERROR, "Unsupported type:%c in walu_call_user_function", paras[i]);
-                    return 0;
+                    return fci;
 
             }
         }
@@ -446,30 +376,38 @@ int luoxin_call_user_method(zval** retval, zval* obj, char* function_name, char*
     fci.params = parameters;
     fci.no_separation =1;
     fci.symbol_table = NULL;
-    fun_re = zend_call_function(&fci, NULL TSRMLS_CC);
+    zend_call_function(&fci, NULL TSRMLS_CC);
 
     //函数调用结束。
     // if(retval_is_null ==1)
     // {
     //     zval_ptr_dtor(retval);
-    //             efree(retval);
+    //     efree(retval);
     // }
 
-    // zval_ptr_dtor(&_function_name);
+    zval_ptr_dtor(&_function_name);
 
     // //free掉parameter及其里面的每个元素zval**，及每个元素zval**对应的zval*
     // //对于传进来的zval，不进行free，由参数调用者自行free
-    // if(paras_count >0)
-    // {
-    //     for(i=0; i<paras_count; i++)
-    //     {
-    //         if(paras[i] !='z')
-    //         {
-    //            zval_ptr_dtor(parameters[i]);
-    //         }
-    //         efree(parameters[i]);
-    //     }
-    //     efree(parameters);
-    // }
-    return fun_re;
+    if(paras_count >0)
+    {
+        for(i=0; i<paras_count; i++)
+        {
+            if(paras[i] !='z')
+            {
+               zval_ptr_dtor(parameters[i]);
+            }
+            efree(parameters[i]);
+        }
+        efree(parameters);
+    }
+    return fci;
 }
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
